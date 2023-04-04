1bdf61a36bfe83e8c28f921853f7040fb9da8b71
Kuma Original <Kuma_Original@gmail.com>
2023-04-04 12:22:48 +0600

    Работа с QuerySet и менеджерами

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Post
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
@@ -11,5 +12,3 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
-
-
