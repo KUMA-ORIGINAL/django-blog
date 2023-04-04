e6a05783602a233691554b5786695426d6a2b235
Kuma Original <Kuma_Original@gmail.com>
2023-04-04 19:07:22 +0600

    Функция «Поделиться статьей через e-mail» / начало 2 главы

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -31,3 +33,25 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Получение статьи по идентификатору
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # Форма была отправлена на сохранение.
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Все поля формы прошли валидацию
+            cd = form.cleaned_data
+            # ... Отправка электронной почты.
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} ({cd['email']}) recommends you reading {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']} comments: {cd['comments']}"
+            send_mail(subject, message, 'thefresh.friends.software@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+                  {'post': post, 'form': form, 'sent': sent})
